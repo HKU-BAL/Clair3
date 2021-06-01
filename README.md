51bfd3cd77141e6ba1ecacaab6960e9ce5d9f206
zhengzhenxian <920596906@qq.com>
2021-06-01 13:57:29 +0800

    update model_path comments

--- README.md
+++ README.md
@@ -259,7 +259,7 @@ docker run -it hkubal/clair3:"${BIN_VERSION}" /opt/bin/run_clair3.sh --help
 ```bash
   -b, --bam_fn=FILE        BAM file input. The input file must be samtools indexed.
   -f, --ref_fn=FILE        FASTA reference file input. The input file must be samtools indexed.
-  -m, --model_path=STR     The folder path containing a Clair3 model (requiring six files in the folder, including pileup.data-00000-of-00001, pileup.index, pileup.meta, full_alignment.data-00000-of-00001, full_alignment.index, and full_alignment.meta).
+  -m, --model_path=STR     The folder path containing a Clair3 model (requiring six files in the folder, including pileup.data-00000-of-00002, pileup.data-00001-of-00002 pileup.index, full_alignment.data-00000-of-00002, full_alignment.data-00001-of-00002  and full_alignment.index).
   -t, --threads=INT        Max threads to be used. The full genome will be divided into small chunks for parallel processing. Each chunk will use 4 threads. The chunks being processed simultaneously is ceil($threads/4)*3. 3 is the overloading factor.
   -p, --platform=STR       Select the sequencing platform of the input. Possible options: {ont,hifi,ilmn}.
   -o, --output=PATH        VCF/GVCF output directory.
--- run_clair3.sh
+++ run_clair3.sh
@@ -13,7 +13,7 @@ print_help_messages()
     echo $'Required parameters:'
     echo $'  -b, --bam_fn=FILE        BAM file input. The input file must be samtools indexed.'
     echo $'  -f, --ref_fn=FILE        FASTA reference file input. The input file must be samtools indexed.'
-    echo $'  -m, --model_path=STR     The folder path containing a Clair3 model (requiring six files in the folder, including pileup.data-00000-of-00001, pileup.index, pileup.meta, full_alignment.data-00000-of-00001, full_alignment.index, and full_alignment.meta).'
+    echo $'  -m, --model_path=STR     The folder path containing a Clair3 model (requiring six files in the folder, including pileup.data-00000-of-00002, pileup.data-00001-of-00002 pileup.index, full_alignment.data-00000-of-00002, full_alignment.data-00001-of-00002  and full_alignment.index).'
     echo $'  -t, --threads=INT        Max #threads to be used. The full genome will be divided into small chunks for parallel processing. Each chunk will use 4 threads. The #chunks being processed simultaneously is ceil(#threads/4)*3. 3 is the overloading factor.'
     echo $'  -p, --platform=STR       Select the sequencing platform of the input. Possible options: {ont,hifi,ilmn}.'
     echo $'  -o, --output=PATH        VCF/GVCF output directory.'
