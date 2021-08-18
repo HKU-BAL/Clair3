f21bce7a25926838db680a1e9503c528fccc28ca
Filipe Tostevin <filipe.tostevin@nanoporetech.com>
2021-08-18 11:20:48 +0100

    output current best val_loss model separately from models at each epoch

--- clair3/Train.py
+++ clair3/Train.py
@@ -213,6 +213,7 @@ def train_model(args):
     )
     early_stop_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, mode="min")
     model_save_callback = tf.keras.callbacks.ModelCheckpoint(ochk_prefix + ".{epoch:02d}", period=1, save_weights_only=False)
+    model_best_callback = tf.keras.callbacks.ModelCheckpoint("best_val_loss", monitor='val_loss', save_best_only=True, mode="min")
 
     # Use first 20 element to initialize tensorflow model using graph mode
     output = model(np.array(table_dataset_list[0].root.position_matrix[:20]))
@@ -232,7 +233,9 @@ def train_model(args):
     train_history = model.fit(x=train_dataset,
                               epochs=max_epoch,
                               validation_data=validate_dataset,
-                              callbacks=[early_stop_callback, model_save_callback],
+                              callbacks=[early_stop_callback,
+                                         model_save_callback,
+                                         model_best_callback],
                               verbose=1,
                               shuffle=False)
 
