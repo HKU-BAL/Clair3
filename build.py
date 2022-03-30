5a4a58c361e90b91ae46fa45876e4e3622c82ce0
zxzheng <zxzheng@cs.hku.hk>
2022-03-31 02:01:27 +0800

    move samtools and longphse compile to Makefile

--- build.py
+++ build.py
@@ -4,36 +4,8 @@ import platform
 from subprocess import run
 from cffi import FFI
 
-
 samver = "1.10"
-longphase_version = "1.0"
 file_directory = os.path.dirname(os.path.realpath(__file__))
-def compile_samtools_package():
-    # just a simple way to compile samtools htslib
-    if not os.path.exists(os.path.join(file_directory, 'libhts.a')):
-        samtools_source = "samtools-{}.tar.bz2 https://github.com/samtools/samtools/releases/download/{}/samtools-{}.tar.bz2".format(samver, samver, samver)
-        run("curl -L -o {}".format(samtools_source), shell=True)
-        run("tar -xjf samtools-{}.tar.bz2".format(samver), shell=True)
-        run("rm samtools-{}.tar.bz2".format(samver), shell=True)
-        run("cd samtools-{} && autoheader && autoconf -Wno-syntax && CFLAGS='-fpic -O3' ./configure && make".format(samver), shell=True)
-        run("cp samtools-{}/htslib-{}/libhts.a {}".format(samver, samver, file_directory), shell=True)
-
-
-def compile_longphase_package():
-    if not os.path.exists(os.path.join(file_directory, 'longphase')):
-        longphase_source = "https://github.com/twolinin/longphase/archive/refs/tags/v{}.tar.gz".format(longphase_version)
-        run("wget {}".format(longphase_source), shell=True)
-        run("tar -zxvf v{}.tar.gz".format(longphase_version), shell=True)
-        run("rm v{}.tar.gz".format(longphase_version), shell=True)
-        run("cd longphase-{} && autoreconf -i && ./configure && make -j4".format(longphase_version), shell=True)
-        run("mv longphase-{}/longphase {}".format(longphase_version, file_directory), shell=True)
-        run("rm -r longphase-{}".format(longphase_version), shell=True)
-
-def clean_samtools_package():
-    # after ffi building, clean the samtools htslib source
-    if os.path.exists(os.path.join(file_directory, 'libhts.a')):
-        run("rm -r samtools-{}".format(samver), shell=True)
-
 htslib_dir=os.path.join(file_directory, 'samtools-{}'.format(samver), 'htslib-{}'.format(samver))
 
 libraries=['m', 'z', 'lzma', 'bz2', 'pthread', 'curl', 'crypto']
@@ -91,7 +63,6 @@ ffibuilder.cdef('\n\n'.join(cdef))
 
 
 if __name__ == "__main__":
-    compile_samtools_package()
-    compile_longphase_package()
     ffibuilder.compile(verbose=True)
-    clean_samtools_package()
+    run("cp {}/libclair3*.so {}/libclair3.so".format(file_directory, file_directory), shell=True)
+
