4326373788d78d5a1db38f2d9764ab4dafb1e0ff
zhengzhenxian <920596906@qq.com>
2023-07-11 12:09:03 +0800

    updated header

--- shared/utils.py
+++ shared/utils.py
@@ -274,10 +274,10 @@ def get_header(reference_file_path=None, cmd_fn=None, sample_name="SAMPLE", vers
         ##INFO=<ID=F,Number=0,Type=Flag,Description="Result from full-alignment calling">
         ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
         ##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype Quality">
-        ##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Read Depth">
-        ##FORMAT=<ID=AD,Number=R,Type=Integer,Description="Read depth for each allele">
-        ##FORMAT=<ID=PL,Number=G,Type=Integer,Description="Phred-scaled genotype likelihoods rounded to the closest integer">
-        ##FORMAT=<ID=AF,Number=1,Type=Float,Description="Estimated allele frequency in the range of [0,1], AF represents reference base allele frequency when $FILTER is RefCall">\n""".format(
+        ##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Approximate read depth (reads with MQ<20 or selected by 'samtools view -F 2316' are filtered)">
+        ##FORMAT=<ID=AD,Number=R,Type=Integer,Description="Allelic depths for the ref and alt alleles in the order listed">
+        ##FORMAT=<ID=PL,Number=G,Type=Integer,Description="Normalized, Phred-scaled likelihoods for genotypes as defined in the VCF specification">
+        ##FORMAT=<ID=AF,Number=1,Type=Float,Description="Observed allele frequency in reads, for each ALT allele, in the same order as listed, or the REF allele for a RefCall">\n""".format(
         version))
 
     if ref_header_str != "":
